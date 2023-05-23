852552603af1cf071020480f433b706cf64968fd
Ali <spailqsd@gmail.com>
2023-05-23 20:34:04 +0300

    task 3

--- tasks/3/main.go
+++ tasks/3/main.go
@@ -5,6 +5,11 @@ import (
 	"task3/strings-sort"
 )
 
+/*
+Example of run:
+go run main.go "./test/nums.txt" -n true
+go run main.go "./test/text.txt" -r true -u true
+*/
 func main() {
 	os.Exit(strings_sort.CLI(os.Args[1:]))
 }
